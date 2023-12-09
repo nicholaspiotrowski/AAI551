5df4412b6da634200b3f40aefbd316e5fe7157d0
Nicholas Piotrowski <71047019+nicholaspiotrowski@users.noreply.github.com>
2023-12-08 23:22:25 -0500

    Added comments to Veggie.py
    Added comments

--- Veggie.py
+++ Veggie.py
@@ -1,7 +1,11 @@
 # Author: Siyuan Fang, Nicholas Piotrowski
-# Date: November 18th, 2023
+# Date: December 8th, 2023
 """Description:
-
+This file handles all veggie objects in the Captain Veggie game.
+Subclass of FieldInhabitant, veggie objects need functionality for their name and points value.
+Functionality includes get and set name, and a custom str output for the object for an easy-to-understand string
+representing the object
+The Veggie represents a game objective.
 """
 
 from FieldInhabitant import FieldInhabitant
@@ -10,8 +14,8 @@ from FieldInhabitant import FieldInhabitant
 class Veggie(FieldInhabitant):
     def __init__(self, symbol, name, points):
         FieldInhabitant.__init__(self, symbol)
-        self.__name = name
-        self.__points = points
+        self.__name = name      # Name of the veggie
+        self.__points = points  # Points the veggie is worth
 
     def getName(self):
         return self.__name
