604eba77e4ca3acc1546c611dbad2d447969f7b5
Nicholas Piotrowski <71047019+nicholaspiotrowski@users.noreply.github.com>
2023-12-08 23:18:32 -0500

    Update Captain.py
    Added comments

--- Captain.py
+++ Captain.py
@@ -1,7 +1,10 @@
 # Author: Siyuan Fang, Nicholas Piotrowski
-# Date: November 17th, 2023
+# Date: December 8th
 """Description:
-
+This file handles the captain object in the Captain Veggie game.
+Subclass of Creature, the captain object need functionality for their veggies collected.
+Functionality includes a getter and setter, and a way to add new veggies to their list.
+The captain represents the playable character
 """
 
 from Creature import Creature
@@ -10,12 +13,12 @@ from Creature import Creature
 class Captain(Creature):
     def __init__(self, x, y):
         Creature.__init__(self, 'V', x, y)
-        self._veggiesCollected = []
+        self._veggiesCollected = []     # List representing all veggies the captain manages to collect in the game
 
     def addVeggie(self, veg):
         """
         Adds veggie object to veggies collected list
-        :param veg:
+        :param veg: Veggie object that captain managed to get. Contains a name, symbol, and points.
         :type veg: Veggie object
         :return: None
         """
