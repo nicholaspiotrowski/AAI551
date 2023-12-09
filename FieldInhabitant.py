399a799fb4b5a0fd807cb41a73f9f006b9fef66e
Nicholas Piotrowski <71047019+nicholaspiotrowski@users.noreply.github.com>
2023-12-08 23:19:38 -0500

    Added comments to FieldInhabitant.py
    Added comments

--- FieldInhabitant.py
+++ FieldInhabitant.py
@@ -1,13 +1,14 @@
-# Author:
-# Date:
+# Author: Siyuan Fang, Nicholas Piotrowski
+# Date: December 8th, 2023
 """Description:
-
+Superclass used for all field inhabitants objects of the Captain Veggie game
+All inhabitants have a symbol and this file handles that functionality
 """
 
 
 class FieldInhabitant:
     def __init__(self, symbol):
-        self._symbol = symbol
+        self._symbol = symbol   # Symbol is character that represents inhabitant on field graphic output
 
     def getSymbol(self):
         return self._symbol
